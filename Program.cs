dcc4407ab2a7b536cea294b5e3eb1048dbe56566
Miguel dias <mdias1300@gmail.com>
2025-04-25 20:02:59 -0300

    atividade feita

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+string senhaDigitada;
+string senhaEsperada = "123456";
+
+Console.WriteLine("Digite sua senha:");
+senhaDigitada = Console.ReadLine()!;
+
+if (senhaDigitada == senhaEsperada)
+{
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine(" Senha correta");
+}
+else
+{  
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("Senha errada");
+}
+Console.ResetColor();
